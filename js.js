0f509d05ebdec32d58d981712a8ea03a3db3bd9d
XC-Xander-Cage <89819308+XC-Xander-Cage@users.noreply.github.com>
2022-04-27 12:58:54 +0100

    Update js.js

--- js.js
+++ js.js
@@ -42,6 +42,7 @@ function toSubmit(){
     dataType: 'json',
     accepts: 'application/json',
     data: dataString,
+        async: true,
         cache:false;
     success: (data) => console.log(data),
     error: (err) => console.log(err)
